1c4e12be9a40e994254fea05a145fcbbd20f2243
emirtemizyurek <tmzyrk1@gmail.com>
2023-09-27 21:35:54 +0300

    StartActivity.kt codded.

--- app/src/main/java/com/example/xogames/StartActivity.kt
+++ app/src/main/java/com/example/xogames/StartActivity.kt
@@ -1,11 +1,20 @@
 package com.example.xogames
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.xogames.databinding.ActivityStartBinding
 
 class StartActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityStartBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start)
+        binding = ActivityStartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonStart.setOnClickListener {
+            Intent(this@StartActivity, MainActivity::class.java)
+            finish()
+        }
     }
 }
